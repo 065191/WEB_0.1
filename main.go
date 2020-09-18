b2cc80657719fecc2a32129162541f5f5f218e38
0ri0n@mail.ru <1Sadafaga/>
2020-09-18 22:53:10 +0300

    new form 1

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func (m msg) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
 	fmt.Fprint(resp, m)
 }
 func main() {
-	msgHandler := msg("Hello from Web Server in Go")
+	msgHandler := msg("Hello from Web Server in Go!!")
 	fmt.Println("Server is listening...")
 	http.ListenAndServe(":80", msgHandler)
 
