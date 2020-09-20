0ab822dac618fc6c93728582153dd43c29767c91
0ri0n@mail.ru <1Sadafaga/>
2020-09-20 22:41:48 +0300

    + 2 wev test fail

--- main_stroka.go
+++ main_stroka.go
@@ -12,7 +12,7 @@ func main() {
 		name := r.URL.Query().Get("name")
 		age := r.URL.Query().Get("age")
 		nick := r.URL.Query().Get("nick")
-		fmt.Fprintf(w, "Имя: %s Возраст: %s,nick: %s", name, age, nick)
+		fmt.Fprintf(w, "Имя: %s \nВозраст: %s \nnick: %s\n", name, age, nick)
 	})
 	fmt.Println("Server is listening...")
 	http.ListenAndServe(":88", nil)
